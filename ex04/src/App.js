60953f31ae552b7b1578a1a830f65bc124bf1f2f
Sue-cod <snozic892@gmail.com>
2021-04-02 20:55:51 +0200

    zadnji dan

---
+++ ex04/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import GetRequestSetHeaders from '../../ex04/src/GetRequestSetHeaders';
+function App() {
+  return (
+    <div className="App">
+     <GetRequestSetHeaders />
+    </div>
+  );
+}
+export default App;
